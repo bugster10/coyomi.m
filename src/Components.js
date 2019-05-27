781ab703223afb96e62458ef8e66133099d4d3ab
Trevor <trevor.mangram@nti.co.jp>
2019-05-27 17:38:15 +0900

    Adding styles to intro page

--- src/Components.js
+++ src/Components.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
+// class Test extends React.Component {
+//     render() {
+//         return(
+//             <h1>Hello World</h1>
+//         );
+//     };
+// };
+
 class Test extends React.Component {
     render() {
         return(
-            <h1>Hello World</h1>
+            <div id="intro" className="row">
+                <div id="intro-container"
+                className="container text-white">
+                    <div className="card-body d-flex justify-content-center">
+                        Test
+
+                    </div>
+                    
+                </div>
+            </div>
         );
     };
 };
